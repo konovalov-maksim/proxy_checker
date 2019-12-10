6734c2cf6d4e2f7e258aef07dc8f38d64e1d4ae7
Maksim <konovalov.ms93@gmail.com>
2019-12-10 22:34:27 +0300

    Доработана логика провреки прокси

--- src/main/java/proxyChecker/processor/Checker.java
+++ src/main/java/proxyChecker/processor/Checker.java
@@ -1,17 +1,14 @@
 package proxyChecker.processor;
 
-import okhttp3.*;
-import org.jetbrains.annotations.NotNull;
-
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.List;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
 
-public class Checker {
+public class Checker{
     private int maxThreads = 1;
+    private int threads;
     private int checksCount;
     private int timeout = 5000;
     private String url;
@@ -31,30 +28,50 @@ public class Checker {
     private void createLoaders() {
         for (int i = 0; i < checksCount; i++)
             for (ExtendedProxy extProxy : proxies)
-                loaders.add(new Loader(extProxy));
+                loaders.add(new Loader(extProxy, timeout, url));
     }
 
-    private void startNewChecks() {
-        OkHttpClient client = new OkHttpClient.Builder()
-                .build();
-
-        Request request = new Request.Builder()
-                .url(url).build();
-
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                e.printStackTrace();
-            }
-
-            @Override
-            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-//                results.indexOf()
-            }
-        });
+    private void runNewLoader() {
+        while (loaders.size() > 0 && threads < maxThreads) {
+            loaders.pop().run();
+            threads++;
+        }
     }
 
     public interface loadingListener {
-        public void onResponse();
+        void onResponse();
     }
+
+    public int getMaxThreads() {
+        return maxThreads;
+    }
+
+    public void setMaxThreads(int maxThreads) {
+        this.maxThreads = maxThreads;
+    }
+
+    public int getChecksCount() {
+        return checksCount;
+    }
+
+    public void setChecksCount(int checksCount) {
+        this.checksCount = checksCount;
+    }
+
+    public int getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(int timeout) {
+        this.timeout = timeout;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
 }
--- src/main/java/proxyChecker/processor/Loader.java
+++ src/main/java/proxyChecker/processor/Loader.java
@@ -1,20 +1,49 @@
 package proxyChecker.processor;
 
-public class Loader {
-    private ExtendedProxy extProxy;
+import okhttp3.*;
+import org.jetbrains.annotations.NotNull;
 
-    public Loader(ExtendedProxy extProxy) {
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+class Loader implements Callback {
+    private ExtendedProxy extProxy;
+    private OkHttpClient client;
+    private Request request;
+
+    Loader(ExtendedProxy extProxy, long timeout, String url) {
         this.extProxy = extProxy;
-//        if (extProxy.getLogin() != null && extProxy.getPass() != null)
-//            loaders.add(new OkHttpClient.Builder()
-//                    .proxy(extProxy.getProxy())
-//                    .authenticator(extProxy.getAuth())
-//                    .callTimeout(timeout, TimeUnit.MILLISECONDS)
-//                    .build());
-//        else
-//            loaders.add(new OkHttpClient.Builder()
-//                    .proxy(extProxy.getProxy())
-//                    .callTimeout(timeout, TimeUnit.MILLISECONDS)
-//                    .build());
+        if (extProxy.getLogin() != null && extProxy.getPass() != null)
+            client = new OkHttpClient.Builder()
+                    .proxy(extProxy.getProxy())
+                    .authenticator(extProxy.getAuth())
+                    .callTimeout(timeout, TimeUnit.MILLISECONDS)
+                    .build();
+        else client = new OkHttpClient.Builder()
+                .proxy(extProxy.getProxy())
+                .callTimeout(timeout, TimeUnit.MILLISECONDS)
+                .build();
+        request = new Request.Builder()
+                .url(url)
+                .build();
+    }
+
+    void run() {
+        client.newCall(request).enqueue(this);
+    }
+
+    @Override
+    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+        extProxy.addResponseCode(408);
+        extProxy.addResponseTime(0L);
+    }
+
+    @Override
+    public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+
+    }
+
+    interface loadingListener {
+
     }
 }
